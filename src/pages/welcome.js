3313b3efc41ecbbb2626e6def486e5605bdbf320
RikvanderSchalie <79224832+RikvanderSchalie@users.noreply.github.com>
2021-07-05 11:28:05 +0200

    Background

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import "./welcome.css";
-
+import foto from"./vintage2.jpg";
 function WelkomPage ()
 
 {
     return(
     <div className="App">
         <header className="App-header">
-            <a className="App-link"
-            >
+            <img src={foto} alt="testbla" className= "Background"/>
+            <a className="App-link">
                 Welkom 2 Memberberry
             </a>
         </header>
