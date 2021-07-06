28d5641edcb852edc12381d85ce7235cac3173ac
RikvanderSchalie <79224832+RikvanderSchalie@users.noreply.github.com>
2021-07-06 12:18:42 +0200

    ad grid an button function test

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -5,13 +5,13 @@ function WelkomPage ()
 
 {
     return(
-    <div className="test">
+    <div className="grid-container">
 
-        <shape className="shape">
+        <shape className="shape-green" id="grid-item" >
             <h1 className="titeltekst">Memberberry</h1>
             <h3 className="welcome">Welcome</h3>
             <h3 className="letsmake">Lets make some lists !</h3>
-            <p><button className="button"></button></p>
+            <p><button className="button" onClick={() => console.log ("BOEM!!")}  ></button></p>
         </shape>
     </div>
 
