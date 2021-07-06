35b5daf9f379f1766f9902abe7ff206e9a79d8e0
RikvanderSchalie <79224832+RikvanderSchalie@users.noreply.github.com>
2021-07-06 15:44:38 +0200

    killed it

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -5,18 +5,21 @@ function WelkomPage ()
 
 {
     return(
-    <div className="grid-container">
+        <div className="test">
+            <div className="grid-container">
 
-        <shape className="shape-green" id="grid-item" >
-            <h1 className="titeltekst">Memberberry</h1>
-            <h3 className="welcome">Welcome</h3>
-            <h3 className="letsmake">Lets make some lists !</h3>
-            <p><button className="button" onClick={() => console.log ("BOEM!!")}  ></button></p>
-        </shape>
-    </div>
+                <shape className="shape"></shape>
+                    <shape className="shape-green" id="grid-item" >
+                        <h1 className="titeltekst">Memberberry</h1>
+                        <h3 className="welcome">Welcome</h3>
+                        <h3 className="letsmake">Lets make some lists !</h3>
+                        <p><button className="button"></button></p>
+                        <p><button className="button" onClick={() => console.log ("BOEM!!")}  ></button></p>
+                    </shape>
+            </div>
 
 
-    );
-}
+            );
+            }
 
-export default WelkomPage ;
+            export default WelkomPage ;
