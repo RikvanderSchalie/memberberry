6df8cac5385565a4c68656e3a22c5c2dedefc140
RikvanderSchalie <79224832+RikvanderSchalie@users.noreply.github.com>
2021-07-09 12:23:27 +0200

    sign-up inputs

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -12,13 +12,29 @@ function SignUpPage ()
                <h1 className="titeltekst1">sign up</h1>
                <h3 className="member1">Member ? <Link to="/login">let's sign in</Link></h3>
 
-               <label htmlFor="email-field">
+               <label htmlFor="email-field" className="signup1">
                    <input
                        type="email"
-                       className="signup"
+                       className="signup1"
 /*                     id="email-field"
                        name="email"*/
-                       placeholder="  &#9993; Email"
+                       placeholder="  &#128232;   Email"
+                   />
+               </label>
+
+               <label htmlFor="password-field" className="password1">
+                   <input
+                       type="password"
+                       className="password1"
+                       placeholder="  &#128272;   Password"
+                   />
+               </label>
+
+               <label htmlFor="password-field" className="password2">
+                   <input
+                       type="password"
+                       className="password2"
+                       placeholder="  &#128272;   Repeat password"
                    />
                </label>
 
