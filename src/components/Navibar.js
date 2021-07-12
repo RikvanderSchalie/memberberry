6caff04c57213b9248550e2ac0519e0bf441ad78
RikvanderSchalie <79224832+RikvanderSchalie@users.noreply.github.com>
2021-07-12 12:09:32 +0200

    add buttons to home

--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -1,12 +1,29 @@
 import React from "react";
 import Navibarstyle from "../components/Navibarstyle.css";
+
+function clicktest(){
+    console.log("test!")
+}
+
 function navibar(){
     return (
+
         <div className="navibar">
+
 
             <button
                 className="navbuthome"
-                /*onClick={() => console.log ("BOEMM!!") }*/
+                /*&#127968;*/
+            ></button>
+
+            <button
+                className="navbutlist"
+                /*&#128195;*/
+            >&#128195;</button>
+
+            <button
+                className="navbutset"
+                /*&#9881;*/
             >Go</button>
 
         </div>
