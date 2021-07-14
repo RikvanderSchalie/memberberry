e4a2b01c644586781f7a4a054e075319ac43fd0b
RikvanderSchalie <79224832+RikvanderSchalie@users.noreply.github.com>
2021-07-14 19:59:39 +0200

    import firebase

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import "./Signinstyle.css";
-import { Link } from 'react-router-dom';
 
 /*components*/
 import Buttonsign from "../components/Buttonsign";
 import Inputemail from "../components/Inputemail";
 import Inputpassw from "../components/Inputpassw";
 import Inputpassrep from "../components/Inputpassrep";
+
+import React from 'react';
+import "./Signinstyle.css";
+import { Link } from 'react-router-dom';
+import firebase from 'firebase'
+
+
+
+const app = firebase.initializeApp({
+
+    apiKey: "AIzaSyDavBMrb2NIs0Cc-OgYClzHmNyU-LEoz4Y",
+    authDomain: "memberberry-b2c5a.firebaseapp.com",
+    projectId: "memberberry-b2c5a",
+    storageBucket: "memberberry-b2c5a.appspot.com",
+    messagingSenderId: "648141006624",
+    appId: "1:648141006624:web:2676ef2a25bfed49b39945",
+    measurementId: "G-7BCCV5X38F"
+
+})
+
 
 
 
