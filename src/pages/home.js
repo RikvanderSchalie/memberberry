d681be5e0d7c4857c66cc89ad8a31a15c7b36898
RikvanderSchalie <79224832+RikvanderSchalie@users.noreply.github.com>
2021-07-16 13:04:57 +0200

    search function  ongoing

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,15 +6,33 @@ import { Link } from 'react-router-dom';
 
     function HomePage () {
         const [movies, setMovies] = useState([]);
+
         console.log("WHAT IS THE STATE:", movies);
 
+// Zoekbalk films
     window.onload = function(){
         const searchbar = document.getElementById('searchbar')
-        searchbar.addEventListener('keyup',(e) => {
-
-    return(e.target.value);
+        searchbar.addEventListener('keydown',(e) => {
+            if (13 === e.keyCode) { console.log (e.target.value) }
     });
+
 }
+
+//aanroep api
+        useEffect(() => {
+            console.log("ON MOUNT:");
+            async function fetchMovies() {
+                const response = await axios.get(
+                    /* 'http://www.omdbapi.com/?s=fun&apikey=94c16227'*/
+                    'http://www.omdbapi.com/?apikey=94c16227&s=fun'
+                );
+                console.log(response.data.Search);
+                setMovies(response.data.Search); // trigger rerender
+            }
+
+            fetchMovies();
+        }, []);
+
 
     /* dropdownfunctie*/
     function myFunction() {
@@ -35,19 +53,7 @@ import { Link } from 'react-router-dom';
     }
     }
 
-    useEffect(() => {
-        console.log("ON MOUNT:");
-        async function fetchMovies() {
-        const response = await axios.get(
-        'http://www.omdbapi.com/?s=fun&apikey=94c16227'
-    );
 
-        console.log(response.data.Search);
-        setMovies(response.data.Search); // trigger rerender
-        }
-
-        fetchMovies();
-    }, []);
 
     return(
     <>
@@ -61,7 +67,7 @@ import { Link } from 'react-router-dom';
             <div id="myDropdown" className="dropdown-content">
                 <a href="#">Lists</a>
                 <a href="#">Settings</a>
-                <a href="#">Log-in</a>
+                <a href="#">Log-out</a>
             </div>
     </div>
 
